cb56432015f2b8cedefb0c6e334d9d0e9f7008d8
Azimuth00 <thegitterazimuth@gmail.com>
2025-01-06 22:46:50 +0700

    v06011

--- Media/Scripts/menu.gd
+++ Media/Scripts/menu.gd
@@ -1,18 +1,31 @@
 extends Control
 
-
+#var button_array = [$CenterButtons/Button,$CenterButtons/Button2,$CenterButtons/Button3,$CenterButtons/Button4]
 
 func _process(delta: float) -> void:
 	var viewport_size = get_viewport_rect().size
 	var k = ((viewport_size.x/1152)+(viewport_size.y/648))/4
+	var vector2k = Vector2(k,k)
 	size = viewport_size
 	$Background.size = viewport_size
-	$Background2.scale = Vector2(k,k) * 2
-	print(k)
-	$Header.scale = Vector2(k,k)
+	$Header.scale = vector2k
+	$CenterButtons.scale = vector2k * 2
 	
-	var button_array = [$CenterButtons/Button,$CenterButtons/Button2,$CenterButtons/Button3,$CenterButtons/Button4]
 	
-	for i in button_array:
-		#i.scale = Vector2(k,k)*2
-		pass
+	
+
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
